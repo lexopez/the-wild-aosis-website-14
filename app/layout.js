80ea65d60a2ab18365e2181ea126cbc287f7c0bf
alexander lopez <iamlexlopez@gmail.com>
2026-01-10 16:02:55 +0800

    set up header navigation links
    add header, logo, and navigation components. And update layout

--- app/layout.js
+++ app/layout.js
@@ -6,6 +6,7 @@ const josefin = Josefin_Sans({
 });
 
 import "@/app/_styles/globals.css";
+import Header from "./_components/Header";
 
 export const metadata = {
   title: {
@@ -22,6 +23,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col relative`}
       >
+        <Header />
         <div className="flex-1 px-8 py-12 grid">
           <main className="max-w-7xl mx-auto w-full">{children}</main>
         </div>
