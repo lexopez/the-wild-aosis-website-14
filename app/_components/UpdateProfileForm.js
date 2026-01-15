8501a56f71ece2714a61242c4adc6f2eeee87b96
alexander lopez <iamlexlopez@gmail.com>
2026-01-15 23:45:38 +0800

    allow user to update their profile

--- app/_components/UpdateProfileForm.js
+++ app/_components/UpdateProfileForm.js
@@ -1,12 +1,24 @@
+"use client";
+
+import { useState } from "react";
+import { updateGuest } from "../_lib/actions";
 import SubmitButton from "./SubmitButton";
 
-function UpdateProfileForm({ children }) {
+function UpdateProfileForm({ guest, children }) {
+  const [count, setCount] = useState();
+
+  const { fullName, email, nationality, nationalID, countryFlag } = guest;
+
   return (
-    <form className="bg-primary-900 py-8 px-12 text-lg flex gap-6 flex-col">
+    <form
+      action={updateGuest}
+      className="bg-primary-900 py-8 px-12 text-lg flex gap-6 flex-col"
+    >
       <div className="space-y-2">
         <label>Full name</label>
         <input
           disabled
+          defaultValue={fullName}
           name="fullName"
           className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm disabled:cursor-not-allowed disabled:bg-gray-600 disabled:text-gray-400"
         />
@@ -16,6 +28,7 @@ function UpdateProfileForm({ children }) {
         <label>Email address</label>
         <input
           disabled
+          defaultValue={email}
           name="email"
           className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm disabled:cursor-not-allowed disabled:bg-gray-600 disabled:text-gray-400"
         />
@@ -24,7 +37,11 @@ function UpdateProfileForm({ children }) {
       <div className="space-y-2">
         <div className="flex items-center justify-between">
           <label htmlFor="nationality">Where are you from?</label>
-          <img alt="Country flag" className="h-5 rounded-sm" />
+          <img
+            src={countryFlag}
+            alt="Country flag"
+            className="h-5 rounded-sm"
+          />
         </div>
 
         {children}
@@ -33,13 +50,14 @@ function UpdateProfileForm({ children }) {
       <div className="space-y-2">
         <label htmlFor="nationalID">National ID number</label>
         <input
+          defaultValue={nationalID}
           name="nationalID"
           className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm"
         />
       </div>
 
       <div className="flex justify-end items-center gap-6">
-        <SubmitButton>Update profile</SubmitButton>
+        <SubmitButton pendingLabel="Updating...">Update profile</SubmitButton>
       </div>
     </form>
   );
