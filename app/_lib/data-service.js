c875d5e4cd6de939beec0aca0e0d0a4732cd757b
alexander lopez <iamlexlopez@gmail.com>
2026-01-15 19:42:59 +0800

    Update data-service.js

--- app/_lib/data-service.js
+++ app/_lib/data-service.js
@@ -8,9 +8,6 @@ export async function getCabin(id) {
     .eq("id", id)
     .single();
 
-  // For testing
-  // await new Promise((res) => setTimeout(res, 2000));
-
   if (error) {
     console.error(error);
     notFound();
