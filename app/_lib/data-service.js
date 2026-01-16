1703b7bd16e51787392377ea3d948ffd0ea807fa
alexander lopez <iamlexlopez@gmail.com>
2026-01-16 16:49:53 +0800

    user can view its reserve cabins on the account page

--- app/_lib/data-service.js
+++ app/_lib/data-service.js
@@ -105,3 +105,21 @@ export async function getCountries() {
     throw new Error("Could not fetch countries");
   }
 }
+
+export async function getBookings(guestId) {
+  const { data, error, count } = await supabase
+    .from("bookings")
+    // We actually also need data on the cabins as well. But let's ONLY take the data that we actually need, in order to reduce downloaded data.
+    .select(
+      "id, created_at, startDate, endDate, numNights, numGuests, totalPrice, guestId, cabinId, cabins(name, image)"
+    )
+    .eq("guestId", guestId)
+    .order("startDate");
+
+  if (error) {
+    console.error(error);
+    throw new Error("Bookings could not get loaded");
+  }
+
+  return data;
+}
